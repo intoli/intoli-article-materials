553e3b53af4d23c434e8862a70a1ff1845c153e6
Andre Perunicic <perunicic@gmail.com>
2018-03-04 16:24:12 -0500

    Add essential frontend build requirements

---
+++ articles/keras-weight-transfer/frontend/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+const config = {
+  devServer: {
+    clientLogLevel: 'info',
+    contentBase: './frontend',
+    historyApiFallback: true,
+    overlay: {
+      errors: true,
+      warnings: false,
+    },
+    port: 3000,
+    publicPath: '/',
+    stats: {
+      modules: false,
+      chunks: false,
+    },
+  },
+  devtool: 'cheap-module-source-map',
+  entry: path.join(__dirname, 'src', 'index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.json$/,
+        exclude: /node_modules/,
+        loader: 'json-loader',
+      },
+      {
+        test: /\.(eot|svg|otf|ttf|woff|woff2)$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/index.html',
+    }),
+  ],
+  watchOptions: {
+    ignored: /build/,
+  },
+};
+
+
+module.exports = config;
