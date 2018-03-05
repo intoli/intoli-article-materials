6d04da840c7af3d39ee35844bbfdb14e0c46ecc4
Andre Perunicic <perunicic@gmail.com>
2018-03-05 10:33:21 -0500

    Add frontend script that predicts the sample's class

--- articles/keras-weight-transfer/frontend/src/index.js
+++ articles/keras-weight-transfer/frontend/src/index.js
@@ -1,1 +1,34 @@
-console.log('Hello, world.');
+import { Model } from 'keras-js';
+import sample from './sample';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.write('Loading...');
+});
+
+// Make sure to copy model.bin to the public directory.
+const model = new Model({
+  filepath: 'model.bin',
+});
+
+// Perform a prediction and write the results to the console.
+model.ready()
+  .then(() => model.predict({
+    input: new Float32Array(sample),
+  }))
+  .then(({ output }) => {
+    let predictionProbability = -1;
+    let predictedDigit = null;
+    Object.entries(output).forEach(([digit, probability]) => {
+      if (probability > predictionProbability) {
+        predictionProbability = probability;
+        predictedDigit = digit;
+      }
+    });
+    document.write(
+      `Predicted ${predictedDigit} with probability ${predictionProbability.toFixed(3)}.`,
+    );
+  })
+  .catch((error) => {
+    console.log(error);
+  });
