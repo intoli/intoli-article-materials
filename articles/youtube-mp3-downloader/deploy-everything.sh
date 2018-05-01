91d2c918b190d83535c055eda66dd0d6e3702678
Evan Sangaline <esangaline@gmail.com>
2018-05-01 13:34:31 -0400

    Rename the deployment script for the youtube downloader lambda functions.

--- articles/youtube-mp3-downloader/deploy-everything.sh
+++ articles/youtube-mp3-downloader/deploy-everything.sh
@@ -7,6 +7,8 @@ export role_name="YoutubeMp3DownloaderRole"
 export policy_name="YoutubeMp3DownloaderPolicy"
 
 export transcoder_function_name="YoutubeMp3TranscoderFunction"
+export downloader_function_name="YoutubeMp3DownloaderFunction"
+export downloader_api_name="YoutubeDownloaderApi"
 
 
 # Make a new S3 bucket.
@@ -105,3 +107,59 @@ aws lambda update-function-code \
     --function-name "${transcoder_function_name}" \
     --s3-bucket "${bucket_name}" \
     --s3-key youtube-mp3-transcoder.zip
+
+
+# Create the function for the downloader app's API.
+yarn add aws-sdk aws-serverless-express express nunjucks ytdl-core
+zip --symlinks --recurse-paths youtube-mp3-downloader.zip \
+    app.js lambda.js package.json node_modules/
+response="$(aws lambda create-function \
+    --function-name "${downloader_function_name}" \
+    --zip-file fileb://youtube-mp3-downloader.zip \
+    --handler lambda.handler \
+    --runtime nodejs6.10 \
+    --timeout 29 \
+    --role "${role_arn}")"
+echo "${response}"
+downloader_function_arn="$(jq -r .FunctionArn <<< "${response}")"
+
+
+# Query the resources for this API.
+response="$(aws apigateway get-resources \
+    --rest-api-id "${api_id}")"
+echo "${response}"
+root_resource_id="$(jq -r .items[0].id <<< "${response}")"
+
+
+# Create a child resource.
+response="$(aws apigateway create-resource \
+    --rest-api-id "${api_id}" \
+    --parent-id "${root_resource_id}" \
+    --path-part '{proxy+}')"
+echo "${response}"
+proxy_resource_id="$(jq -r .id <<< "${response}")"
+
+
+# Allow GET requests for the proxy resource.
+aws apigateway put-method \
+    --rest-api-id "${api_id}" \
+    --resource-id "${proxy_resource_id}" \
+    --http-method GET \
+    --authorization-type NONE
+
+
+# Integrate the proxy resource with the downloader Lambda function.
+aws apigateway put-integration \
+    --rest-api-id "${api_id}" \
+    --resource-id "${proxy_resource_id}" \
+    --http-method GET \
+    --integration-http-method POST \
+    --type AWS_PROXY \
+    --uri "arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/${transcoder_function_arn}/invocations" \
+    --credentials "${role_arn}"
+
+
+# Deploy the API with a stage name of "v1".
+aws apigateway create-deployment \
+    --rest-api-id "${api_id}" \
+    --stage-name v1
