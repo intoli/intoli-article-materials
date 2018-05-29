e24fb10bf46fafabf3a1be8f8a8a0280fcc44d4a
Evan Sangaline <esangaline@gmail.com>
2018-05-29 16:30:22 -0400

    Add some informative comments for the example test.

--- articles/power-assert/test/test-assertion-errors.js
+++ articles/power-assert/test/test-assertion-errors.js
@@ -1,10 +1,13 @@
 import assert from 'assert';
 
 
+// Note that all of these tests are designed to fail, so we can see the error messages!
 describe('Power Assert Testing Examples', () => {
-  it('fail when an unexpected subtring is found', () => {
+  it('check that an unexpected subtring is not found', () => {
     const result = 'Hello World';
     const unexpectedSubstring = 'World';
+
+    // Jest Equivalent: expect(result).toEqual(expect.not.stringContaining(unexpectedSubstring));
     assert(!result.includes(unexpectedSubstring));
   });
 });
